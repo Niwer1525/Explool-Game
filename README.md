a16ead193ca1b87a2b30995fea19862e0c449761
Niwer1525 <erwin.redote@gmail.com>
2023-11-28 12:17:13 +0100

    Finished the game - Added walls management - Updated README.md - Updated .gitignore - Renamed methods

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ We've create the most basic game I ever seen.
 
 Check in the release section for the latest version of the game in .JAR. Then download it.
 
-You'll need the Java VM (Version 17 or higher). I recommend you to use [OpenJDK](https://openjdk.java.net/install/).
+You'll need the Java VM (Version 17 or higher). I recommend you to use [JRE](https://adoptium.net/temurin/releases/?package=jre).
 
 Finally, double-click on the jar. Enjoy !
--- src/game/SixHumans.java
+++ src/game/SixHumans.java
@@ -6,15 +6,14 @@ import io.Fenetre;
 import io.Random;
 
 /**
- * Programme utilisant un objet de la classe {@link #Fenetre} comme interface
- * graphique.
+ * A small and funny game where you have to move a character to reach an objective and dodging walls.
  */
 public class SixHumans implements Auditeur {
 
 	// Consts
-	private static final String DOSSIER_IMAGES = "img\\";
-	private static final int TAILLE_CASE = 48;
-	private static final int OBSTACLE = -1;
+	private static final String IMAGE_FOLDER = "img\\";
+	private static final int CELL_SIZE = 48;
+	private static final int WALL = -1;
 	private static final int EMPTY = 0;
 	private static final int PLAYER = 1;
 	private static final int OBJECTIVE = 7;
@@ -26,32 +25,57 @@ public class SixHumans implements Auditeur {
 
 	public static void main(String[] args) {
 		/* Creating the window */
-		window = new Fenetre("6 humans", 10 * TAILLE_CASE, 10 * TAILLE_CASE);
-		SixHumans jeu = new SixHumans();
+		window = new Fenetre("6 humans", 10 * CELL_SIZE, 10 * CELL_SIZE);
+		SixHumans game = new SixHumans();
 
-		/* Spawn objects */
-		for(int i = 0; i < 5; i++) jeu.placerObjet(OBSTACLE);
-		jeu.placerObjet(OBJECTIVE);
-		jeu.placerObjet(PLAYER);
-
-		/* Show the grid */
-		jeu.displayGrid();
+		/* Initialize the game */
+		game.init();
 
 		/* Display the frame itself */
-		window.setAuditeur(jeu);
+		window.setAuditeur(game);
 		window.afficher();
+	}
+
+	/**
+	 * Initialize the game
+	 */
+	public void init() {
+		/* Spawn objects */
+		placeObject(OBJECTIVE);
+		placeObject(WALL, 10);
+		int[] playerCoords = placeObject(PLAYER);
+		iPlayer = playerCoords[0];
+		jPlayer = playerCoords[1];
+
+		/* Show the grid */
+		displayGrid();
+	}
+
+	/**
+	 * Place multiple objects on the grid randomly.
+	 * You can not get the coords of each object cause we're limited by the non-oriented object.
+	 * If we were in OOP, we could have created an list (ArrayLit) and save the data into.
+	 * But in our case it's useless and too complicated for students.
+	 * 
+	 * @param elementID The ID of the object to place
+	 * @param count The number of objects to place
+	 */
+	public void placeObject(int elementID, int count) {
+		for(int i = 0; i < count; i++) placeObject(elementID);
 	}
 
 	/**
 	 * Place an object on the grid
 	 * @param elementID The ID of the object to place
 	 */
-	private void placerObjet(int elementID) {
+	private int[] placeObject(int elementID) {
+		int i, j;
 		do {
-			iPlayer = Random.randomize(grid.length - 1);
-			jPlayer = Random.randomize(grid[iPlayer].length - 1);
-		} while (grid[iPlayer][jPlayer] != EMPTY);
-		grid[iPlayer][jPlayer] = elementID;
+			i = Random.randomize(grid.length - 1);
+			j = Random.randomize(grid[i].length - 1);
+		} while (grid[i][j] != EMPTY);
+		grid[i][j] = elementID;
+		return new int[] {i, j};
 	}
 
 	/**
@@ -62,21 +86,21 @@ public class SixHumans implements Auditeur {
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid[i].length; j++) {
 				// If i = 1 et j = 2 -> Then the image should display in (96 ; 48)
-				int x = j * TAILLE_CASE;
-				int y = i * TAILLE_CASE;
+				int x = j * CELL_SIZE;
+				int y = i * CELL_SIZE;
 				// Afficher le sol
-				window.dessinerImage(DOSSIER_IMAGES + "sol.png", x, y, TAILLE_CASE, TAILLE_CASE);
+				window.dessinerImage(IMAGE_FOLDER + "sol.png", x, y, CELL_SIZE, CELL_SIZE);
 				if (grid[i][j] == OBJECTIVE) {
 					// Afficher l'objectif
-					window.dessinerImage(DOSSIER_IMAGES + "objectif.png", x, y, TAILLE_CASE, TAILLE_CASE);
+					window.dessinerImage(IMAGE_FOLDER + "objectif.png", x, y, CELL_SIZE, CELL_SIZE);
 				}
-				if(grid[i][j] == OBSTACLE) {
+				if(grid[i][j] == WALL) {
 					// Afficher l'obstacle
-					window.dessinerImage(DOSSIER_IMAGES + "mur.png", x, y, TAILLE_CASE, TAILLE_CASE);
+					window.dessinerImage(IMAGE_FOLDER + "mur.png", x, y, CELL_SIZE, CELL_SIZE);
 				}
 				if (grid[i][j] == PLAYER) {
 					// Afficher le personnage
-					window.dessinerImage(DOSSIER_IMAGES + "personnage1.png", x, y, TAILLE_CASE, TAILLE_CASE);
+					window.dessinerImage(IMAGE_FOLDER + "personnage1.png", x, y, CELL_SIZE, CELL_SIZE);
 				}
 			}
 		}
@@ -102,10 +126,17 @@ public class SixHumans implements Auditeur {
 	 * @param deltaJ The value on the x axis (If you want to move left, deltaJ = -1 and if you want to move right, deltaJ = 1)
 	 */
 	public void movePlayer(int deltaI, int deltaJ) {
+		/* Get the next cell */
+		int i = (iPlayer + deltaI + grid.length) % grid.length;
+		int j = (jPlayer + deltaJ + grid[iPlayer].length) % grid[iPlayer].length;
+
+		/* If it's a wall, then prevent the movement */
+		if(grid[i][j] == WALL) return;
+		
 		/* Move the player */
-		grid[iPlayer][jPlayer] = 0;
-		iPlayer = (iPlayer + deltaI + grid.length) % grid.length;
-		jPlayer = (jPlayer + deltaJ + grid[iPlayer].length) % grid[iPlayer].length;
-		grid[iPlayer][jPlayer] = 1;
+		grid[iPlayer][jPlayer] = EMPTY;
+		iPlayer = i;
+		jPlayer = j;
+		grid[iPlayer][jPlayer] = PLAYER;
 	}
 }
